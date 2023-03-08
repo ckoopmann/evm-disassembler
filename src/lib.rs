9a95973cc234a0c6a252cd3509fdfb5c2e59f891
ckoopmann <c.k.e.koopmann@gmail.com>
2023-03-08 15:04:55 +0800

    Implement Stop opcode decoding

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,41 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::collections::VecDeque;
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Opcode {
+    Stop,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct Operation {
+    opcode: Opcode,
+    stack_input: Vec<String>,
+}
+
+pub fn disassemble(mut input: &str) -> Vec<Operation> {
+    input = input.trim_start_matches("0x");
+    let mut bytes = VecDeque::from(hex::decode(input).expect("Invalid hex string")); 
+    let mut operations = Vec::new();
+    while bytes.len() > 0 {
+        operations.push(decode_operation(&mut bytes));
+    }
+    return operations;
+}
+
+fn decode_operation(bytes: &mut VecDeque<u8>) -> Operation {
+    match bytes.pop_front() {
+        None => panic!("Unexpected end of input"),
+        Some(value) => match value {
+            0x00 => decode_stop(),
+            _ => panic!("Invalid opcode: {}", value),
+        },
+    }
+}
+
+fn decode_stop() -> Operation {
+    Operation {
+        opcode: Opcode::Stop,
+        stack_input: Vec::new(),
+    }
 }
 
 #[cfg(test)]
@@ -7,8 +43,11 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn decode_stop() {
+        let result = disassemble("0x00");
+        assert_eq!(result, vec![Operation {
+            opcode: Opcode::Stop,
+            stack_input: vec![],
+        }]);
     }
 }
